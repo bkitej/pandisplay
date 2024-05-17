6a74ce58196c3d7c6b7e8f874f99458b8a405baa
bkitej-rw <ben@random-walk.dev>
2024-05-17 15:09:44 -0300

    add imprecise method

--- pandisplay/__init__.py
+++ pandisplay/__init__.py
@@ -1,30 +1,50 @@
 import pandas
+import re
 from IPython.display import display
 
-display_options = {
-    'wide': 'display.max_columns',
-    'long': 'display.max_rows',
-    'uncut': 'display.max_colwidth',
+
+_VALID_METHOD_NAME = re.compile(r'^[a-zA-Z_][a-zA-Z0-9_]*$')
+_OPTION_ALIASES = {
+    'wide': {'display.max_columns': None},
+    'long': {'display.max_rows': None},
+    'uncut': {'display.max_colwidth': None},
+    'imprecise': {'display.precision': 3},
 }
 
-def patch():
-    def _validate(opts):
-        invalid = list(set(opts) - set(display_options))
-        if invalid:
-            raise ValueError(
-                f'{invalid} not in valid display options: {list(display_options)}'
-            )
+
+def patch(**kwargs):
+    def _format_args(opts):
+        formatted = []
+        for opt in opts:
+            if isinstance(opt, dict):
+                items = opt.items()
+            else:
+                items = _OPTION_ALIASES[opt].items()
+            for k, v in items:
+                formatted += [k, v]
+        return formatted
 
     def _display(self, *opts):
-        _validate(opts)
-        options = sum([[display_options[opt], None] for opt in opts], [])
-        with pandas.option_context(*options):
+        if opts:
+            options = _format_args(opts)
+            with pandas.option_context(*options):
+                display(self)
+        else:
             display(self)
-    
+
+    def _is_valid_method_name(name):
+        return bool(_VALID_METHOD_NAME.match(name))
+
     pandas.core.generic.NDFrame.display = _display
-    pandas.core.generic.NDFrame.long = lambda frame: _display(frame, 'long')
-    pandas.core.generic.NDFrame.wide = lambda frame: _display(frame, 'wide')
-    pandas.core.generic.NDFrame.uncut = lambda frame: _display(frame, 'uncut')
-    pandas.core.generic.NDFrame.full = lambda frame: _display(frame, *display_options)
+    pandas.core.generic.NDFrame.full = lambda frame: _display(frame, *_OPTION_ALIASES)
+
+    for method_name, opts in {**_OPTION_ALIASES, **kwargs}.items():
+        if _is_valid_method_name(method_name):
+            # TODO: more robust multi-key custom logic
+            setattr(
+                pandas.core.generic.NDFrame,
+                method_name,
+                lambda frame, opts=opts: _display(frame, opts),
+            )
 
 __all__ = ['patch']
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pandisplay',
-    version='0.1.0',
+    version='2.0.0',
     packages=find_packages(),
     description='Pandas display methods for DataFrame visualization in Jupyter Notebooks.',
     author='bkitej',
